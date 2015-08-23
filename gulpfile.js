ccff055ba1ae6acfbfc327b35f6c4ef3ea26d994
Raul <raulodaguiri@gmail.com>
2015-08-23 02:51:15 -0300

    Set basic grid and reset

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,40 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
+var notify = require('gulp-notify') ;
+var bower = require('gulp-bower');
+
+var config = {
+  bowerDir: './assets/vendor',
+  sassDir: './assets/sass'
+}
+
+// bower
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest(config.bowerDir));
+});
 
 // sass
-gulp.task('sass', function(){
-  gulp
-    .src('./assets/sass/application.sass')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+gulp.task('sass', function() {
+  return gulp
+    .src(config.sassDir + '/application.sass')
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: [
+        config.sassDir,
+        config.bowerDir + '/**'
+      ]
+    })).on('error', notify.onError(function (error) {
+      return 'Error: ' + error.message;
+     }))
     .pipe(concat('application.css'))
     .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('sass:watch', function() {
-  gulp.watch('./assets/sass/**/*.sass', ['sass']);
+  gulp.watch(config.sassDir + '/**/*.sass', ['sass']);
 });
 
 // tasks
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('default', ['bower', 'sass', 'sass:watch']);

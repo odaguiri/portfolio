1587dda363e14a608c49bd28f04543f66ce706d6
Raul <raulodaguiri@gmail.com>
2015-08-23 09:20:37 -0300

    Set browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var notify = require('gulp-notify') ;
 var bower = require('gulp-bower');
+var browserSync = require('browser-sync').create();
 
 var config = {
+  htmlDir: './',
   bowerDir: './assets/vendor',
   sassDir: './assets/sass'
 }
@@ -31,12 +33,23 @@ gulp.task('sass', function() {
       return 'Error: ' + error.message;
      }))
     .pipe(concat('application.css'))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', function() {
-  gulp.watch(config.sassDir + '/**/*.sass', ['sass']);
+// Static Server + watching scss/html files
+gulp.task('server', ['sass'], function() {
+
+    browserSync.init({
+      server: "./"
+    });
+
+    // sass
+    gulp.watch(config.sassDir + '/**/*.sass', ['sass']);
+
+    // html
+    gulp.watch(config.htmlDir + '/index.html').on('change', browserSync.reload);
 });
 
 // tasks
-gulp.task('default', ['bower', 'sass', 'sass:watch']);
+gulp.task('default', ['bower', 'sass', 'server']);

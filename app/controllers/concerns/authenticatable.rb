926a675272871408e26dd673ae978b057d076ae6
Raul Odaguiri <raul@iugu.com>
2016-09-08 10:22:52 -0300

    Remove hardcoded authentication

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -3,7 +3,8 @@ module Authenticatable
 
   def authenticate_user!
     session[:user_signed_in?]= authenticate_or_request_with_http_basic do |username, password|
-      username == 'root' && password == 'root'
+      username == Rails.application.secrets.auth_username &&
+      password == Rails.application.secrets.auth_password
     end
   end
 end

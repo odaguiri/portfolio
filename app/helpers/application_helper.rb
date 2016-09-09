72c497db4555d902332b5798964c9a92211b2383
Raul Odaguiri <raul@iugu.com>
2016-09-09 09:54:37 -0300

    Create full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def full_title(page)
+    title = 'odaga.com.br' 
+    title += ' - ' + page if page.present?
+    title
+  end
+
   def user_signed_in?
     session[:user_signed_in?] === true
   end

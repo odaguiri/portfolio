fac6c726812fe09b72eacc59a130fa0cc838cad6
Raul Odaguiri <raulodaguiri@gmail.com>
2016-10-16 17:33:09 -0200

    Add open graph metas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
   def fb_like(url)
     url = URI.encode url
     fb_app_id = Rails.application.secrets.fb_app_id
-    src = "https://www.facebook.com/plugins/like.php?href=#{url}width=450&layout=standard&action=like&size=small&show_faces=false&share=true&appId=#{fb_app_id}"
+    src = "https://www.facebook.com/plugins/like.php?href=#{url}width=auto&layout=standard&action=like&size=small&show_faces=false&share=true&appId=#{fb_app_id}"
     content_tag(:iframe, nil, src: src, class: 'fb-like', frameborder: 0, allowTransparency: 'true')
   end
 end

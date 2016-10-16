9938e8b4da1cba074cdd75e4d289c5e6c0365e13
Raul Odaguiri <raulodaguiri@gmail.com>
2016-10-16 17:23:03 -0200

    Create section bar and add facebook button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
   def user_signed_in?
     session[:user_signed_in?] === true
   end
+
+  def fb_like(url)
+    url = URI.encode url
+    fb_app_id = Rails.application.secrets.fb_app_id
+    src = "https://www.facebook.com/plugins/like.php?href=#{url}width=450&layout=standard&action=like&size=small&show_faces=false&share=true&appId=#{fb_app_id}"
+    content_tag(:iframe, nil, src: src, class: 'fb-like', frameborder: 0, allowTransparency: 'true')
+  end
 end

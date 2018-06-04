fc780a6526d3d15c3fa950717e5f52fe4c85c03d
Raul Odaguiri <raulodaguiri@gmail.com>
2018-06-03 22:11:38 -0300

    Fix assets precompile and environments

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,8 +6,9 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts') 
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|png|jpg|jpeg|gif)\z/

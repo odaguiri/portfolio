b3914a4a47e8614f2fc18ae71494bb173082fc7d
Raul Odaguiri <raulodaguiri@gmail.com>
2018-06-05 21:47:29 -0300

    Fix scroll transition

--- app/assets/javascripts/components/scroll_transition.js
+++ app/assets/javascripts/components/scroll_transition.js
@@ -2,9 +2,15 @@ function ScrollTransition(el) {
   var was_animate = false;
 
   window.addEventListener('scroll', function(){
-    if(!was_animate &&
-       window.pageYOffset >= (el.offsetTop - 350)) {
+    var pageYOffset = window.pageYOffset;
 
+    if(
+      !was_animate &&
+      (
+        pageYOffset >= (el.offsetTop - 350) ||
+        pageYOffset >= (document.querySelector('.main-body').clientHeight - window.outerHeight)
+      )
+    ){
       was_animate = true;
       _u.addClass(el, 'fx');
     }
